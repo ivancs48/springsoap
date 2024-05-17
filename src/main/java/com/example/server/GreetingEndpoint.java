81acc2a7163bea7ecf456b7ed2104f4eb9cc1a22
Luis Murcia <lmurci8@bancodebogota.com.co>
2024-05-16 21:29:12 -0500

    first commit

---
+++ src/main/java/com/example/server/GreetingEndpoint.java
@@ -0,0 +1,24 @@
+package com.example.server;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+
+@Endpoint
+public class GreetingEndpoint {
+
+    private static final String NAMESPACE_URI = "http://example.com/greeting";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getGreetingRequest")
+    @ResponsePayload
+    public GetGreetingResponse getGreeting(@RequestPayload GetGreetingRequest request) {
+        GetGreetingResponse response = new GetGreetingResponse();
+        String name = request.getName();
+
+       
+
+        response.setGreeting("Hello, " + name + "!");
+        return response;
+    }
+}
